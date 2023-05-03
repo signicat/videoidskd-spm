be288b6a5c102efa892d9911f787aa36aeb3fa9f
Marc Tamarit <ex_marc.tamarit@electronicid.eu>
2023-05-03 17:12:06 +0200

    add dependencies to Targets

--- Package.swift
+++ Package.swift
@@ -3,12 +3,18 @@
 
 import PackageDescription
 
+let dependencies: [Target.Dependency] = [
+    .product(name: "WebRTC", package: "eidwebrtc-spm"),
+    .product(name: "Lottie", package: "lottie-spm"),
+    .product(name: "OpenSSL", package: "OpenSSL")
+]
+
 let package = Package(
     name: "VideoIDSDK",
     products: [
         .library(
             name: "VideoIDSDK",
-            targets: ["VideoIDSDK"]),
+            targets: ["VideoIDSDK", "_VideoIDSDKStub"]),
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.1.3"),
@@ -17,8 +23,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "VideoIDSDK",
-            path: "VideoIDSDK.xcframework"
-        ),
+                    name: "VideoIDSDK",
+                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/1.5.0/VideoIDSDK.xcframework.zip",
+                    checksum: "f4c1c541705709f262fdf22a8ecf6c0a4b80c97db63f6d2cf7b62d3ffeea88e5"),
+        .target(name: "_VideoIDSDKStub",
+               dependencies: dependencies)
     ]
 )
---
+++ Sources/_VideoIDSDKStub/Stub.swift
@@ -0,0 +1,8 @@
+//
+//  VideoIDSDKStub.swift
+//  
+//
+//  Created by Marc Tamarit on 3/5/23.
+//
+
+class VideoIDSDKStub { }
