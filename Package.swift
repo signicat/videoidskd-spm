587e6418eb8db208974cc29fe88f9662d3eb710a
Marc Tamarit <ex_marc.tamarit@electronicid.eu>
2023-05-24 11:27:47 +0200

    set version to root dependencies

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.1.3"),
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.1700")),
-        .package(url: "https://gitlab.electronicid.eu/eid-public/eid-sdk/eidwebrtc-spm", branch: "main")
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "1.1.2000"),
+        .package(url: "https://gitlab.electronicid.eu/eid-public/eid-sdk/eidwebrtc-spm", exact: "1.1.35")
     ],
     targets: [
         .binaryTarget(
