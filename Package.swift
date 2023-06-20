4ed2b046efb69aedf827c53b25a3b3086bc599f3
Marc Tamarit <ex_marc.tamarit@electronicid.eu>
2023-06-20 11:15:57 +0200

    release 1.8.0

--- Package.swift
+++ Package.swift
@@ -17,15 +17,15 @@ let package = Package(
             targets: ["VideoIDSDK", "_VideoIDSDKStub"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.1.3"),
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.0.0"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "1.1.2000"),
         .package(url: "https://gitlab.electronicid.eu/eid-public/eid-sdk/eidwebrtc-spm", from: "1.1.35")
     ],
     targets: [
         .binaryTarget(
                     name: "VideoIDSDK",
-                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/1.7.0/VideoIDSDK.xcframework.zip",
-                    checksum: "1a37de88579417238e6f51c1bd5e979e5a42523cd6009dc915d92c1058522c6b"),
+                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/1.8.0/VideoIDSDK.xcframework.zip",
+                    checksum: "d8ef54e5bd8a1b14113f01fc7c86bf20f2846a64c6b678afa2fe1a6adcd62a1f"),
         .target(name: "_VideoIDSDKStub",
                dependencies: dependencies)
     ]
