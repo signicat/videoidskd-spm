3dbdecfe95089744929630c3fa08e2dbfbc7bcfa
Marc Tamarit <ex_marc.tamarit@electronicid.eu>
2023-10-24 15:17:39 +0200

    remove lottie dependency

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let dependencies: [Target.Dependency] = [
     .product(name: "WebRTC", package: "eidwebrtc-spm"),
-    .product(name: "Lottie", package: "lottie-spm"),
     .product(name: "OpenSSL", package: "OpenSSL")
 ]
 
@@ -17,7 +16,6 @@ let package = Package(
             targets: ["VideoIDSDK", "_VideoIDSDKStub"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.0.0"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "1.1.2000"),
         .package(url: "https://gitlab.electronicid.eu/eid-public/eid-sdk/eidwebrtc-spm", from: "1.1.35")
     ],
