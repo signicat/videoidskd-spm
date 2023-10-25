64e0ed5b425488e5115ec27a8a223467e0d867b3
Marc Tamarit <ex_marc.tamarit@electronicid.eu>
2023-10-25 15:14:30 +0200

    remove lottie linked and downgrade version to 4.0.1

--- Package.swift
+++ Package.swift
@@ -14,10 +14,11 @@ let package = Package(
     products: [
         .library(
             name: "VideoIDSDK",
+            type: .dynamic,
             targets: ["VideoIDSDK", "_VideoIDSDKStub"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.1.3"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.0.1"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "1.1.2000"),
         .package(url: "https://gitlab.electronicid.eu/eid-public/eid-sdk/eidwebrtc-spm", from: "1.1.35")
     ],
@@ -27,9 +28,6 @@ let package = Package(
                     url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/1.8.0/VideoIDSDK.xcframework.zip",
                     checksum: "d8ef54e5bd8a1b14113f01fc7c86bf20f2846a64c6b678afa2fe1a6adcd62a1f"),
         .target(name: "_VideoIDSDKStub",
-                dependencies: dependencies,
-                linkerSettings: [
-                    .linkedFramework("Lottie", .when(platforms: [.iOS]))
-                ])
+                dependencies: dependencies)
     ]
 )
