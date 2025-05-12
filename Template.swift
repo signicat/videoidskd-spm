bf63d5fa02fda14e755dd03bab3936f2930a9e6d
signimarc <marc.tamarit@signicat.com>
2025-05-12 11:19:02 +0200

    add template

---
+++ Template.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version: String = "REPLACE_VERSION"
+let checksum: String = "REPLACE_CHECKSUM"
+
+let dependencies: [Target.Dependency] = [
+    .product(name: "WebRTC", package: "eidwebrtc-spm"),
+    .product(name: "Lottie", package: "lottie-spm"),
+    .product(name: "OpenSSL", package: "OpenSSL")
+]
+
+let package = Package(
+    name: "VideoIDSDK",
+    products: [
+        .library(
+            name: "VideoIDSDK",
+            targets: ["VideoIDSDK", "_VideoIDSDKStub"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.4.3"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.2.2000"),
+        .package(url: "https://github.com/signicat/eidwebrtc-spm", from: "1.1.37")
+    ],
+    targets: [
+        .binaryTarget(
+                    name: "VideoIDSDK",
+                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/\(version)/VideoIDSDK.xcframework.zip",
+                    checksum: checksum),
+        .target(name: "_VideoIDSDKStub",
+                dependencies: dependencies)
+    ]
+)
+
+
+
