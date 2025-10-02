7d7248cf171f3e4cca0b1920bad08aa7c7338f67
signimarc <marc.tamarit@signicat.com>
2025-10-02 13:48:27 +0200

    Upgrade to version 1.38.1

--- Package.swift
+++ Package.swift
@@ -3,18 +3,17 @@
 
 import PackageDescription
 
-let sdkVersion: String = "1.41.0"
-let checksum: String = "287c0f9e75845d84876557dde4a025f658aca3bb74e66288749efddc3e1ddb01"
+let version: String = "1.38.1"
+let checksum: String = "6b59a50d089c9ffe66effd1b333a109df6fabf4d7cf876953a03fc9533e89211"
 
 let dependencies: [Target.Dependency] = [
-    .product(name: "WebRTC", package: "WebRTC"),
-    .product(name: "Lottie", package: "lottie-spm")]
+    .product(name: "WebRTC", package: "eidwebrtc-spm"),
+    .product(name: "Lottie", package: "lottie-spm"),
+    .product(name: "OpenSSL", package: "OpenSSL")
+]
 
 let package = Package(
     name: "VideoIDSDK",
-    platforms: [.iOS("15.1"),
-                .macOS(.v10_15)
-    ],
     products: [
         .library(
             name: "VideoIDSDK",
@@ -22,20 +21,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.4.3"),
-        .package(url: "https://github.com/stasel/WebRTC.git", exact: "134.0.0")
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.2.2000"),
+        .package(url: "https://github.com/signicat/eidwebrtc-spm", from: "1.1.37")
     ],
     targets: [
         .binaryTarget(
                     name: "VideoIDSDK",
-                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/\(sdkVersion)/VideoIDSDK.xcframework.zip",
+                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/\(version)/VideoIDSDK.xcframework.zip",
                     checksum: checksum),
-        .target(
-            name: "_VideoIDSDKStub",
-            dependencies: dependencies,
-            swiftSettings: [
-                .define("PLATFORM_IOS_ONLY", .when(platforms: [.iOS]))
-            ]
-        )
+        .target(name: "_VideoIDSDKStub",
+                dependencies: dependencies)
     ]
 )
+
+
 
