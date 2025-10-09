9bc2662666861641f55eaf6446b638c53e1c6e55
signimarc <marc.tamarit@signicat.com>
2025-10-09 17:27:10 +0200

    Upgrade to version 1.46.0-RC

--- Package.swift
+++ Package.swift
@@ -3,18 +3,17 @@
 
 import PackageDescription
 
-let sdkVersion: String = "1.45.0"
-let checksum: String = "2e566a861bbf6c5a288c66d951118e69a1149e49dd911ae2cb9cf0b6db6bce04"
+let version: String = "1.46.0-RC"
+let checksum: String = "820b020051af9362391e47a4b268bf3cdee1b76287e0d114c9136dad75d5f6f1"
 
 let dependencies: [Target.Dependency] = [
-    .product(name: "WebRTC", package: "WebRTC"),
-    .product(name: "Lottie", package: "lottie-spm")]
+    .product(name: "WebRTC", package: "eidwebrtc-spm"),
+    .product(name: "Lottie", package: "lottie-spm"),
+    .product(name: "OpenSSL", package: "OpenSSL")
+]
 
 let package = Package(
     name: "VideoIDSDK",
-    platforms: [.iOS("15.1"),
-                .macOS(.v10_15)
-    ],
     products: [
         .library(
             name: "VideoIDSDK",
@@ -22,20 +21,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.4.3"),
-        .package(url: "https://github.com/stasel/WebRTC.git", exact: "134.0.0")
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.2.2000"),
+        .package(url: "https://github.com/signicat/eidwebrtc-spm", from: "1.1.37")
     ],
     targets: [
         .binaryTarget(
                     name: "VideoIDSDK",
-                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/\(sdkVersion)/VideoIDSDK.xcframework.zip",
+                    url:"https://eid-librerias-ios.s3.eu-west-1.amazonaws.com/VideoID-sdk/\(version)/VideoIDSDK.xcframework.zip",
                     checksum: checksum),
-        .target(
-            name: "_VideoIDSDKStub",
-            dependencies: dependencies,
-            swiftSettings: [
-                .define("PLATFORM_IOS_ONLY", .when(platforms: [.iOS]))
-            ]
-        )
+        .target(name: "_VideoIDSDKStub",
+                dependencies: dependencies)
     ]
 )
+
+
 
