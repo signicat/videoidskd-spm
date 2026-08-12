2026e98e0153f94838e09c1276468a81b592458b
signimarc <marc.tamarit@signicat.com>
2026-08-12 09:47:25 +0200

    fix: remove Catnip transitive dependency for version 26.10.10

--- Package.swift
+++ Package.swift
@@ -6,10 +6,8 @@ import PackageDescription
 let version: String = "26.10.10-RC"
 let urlVersion: String = "26.10.10"
 let checksum: String = "d79e957d70acfc049c770f33e8af13ece6942f99dba748c8b17c27ecc7f9817a"
-let catnipVersion = Version(0, 0, 5)
 
 let dependencies: [Target.Dependency] = [
-    .product(name: "Catnip", package: "catnip-spm"),
     .product(name: "WebRTC", package: "WebRTC"),
     .product(name: "Lottie", package: "lottie-spm"),
     .product(name: "OpenSSL", package: "OpenSSL")
@@ -26,7 +24,6 @@ let package = Package(
             targets: ["VideoIDSDK", "_VideoIDSDKStub"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/signicat/catnip-spm.git", exact: catnipVersion),
         .package(url: "https://github.com/airbnb/lottie-spm.git", exact: "4.4.3"),
         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.2.2000"),
         .package(url: "https://github.com/stasel/WebRTC.git", exact: "134.0.0")
