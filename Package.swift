f886abfc637aa1326b474d2e199b3eb9b403de69
GitLab CI <ci@electronicid.eu>
2026-08-18 09:56:32 +0200

    Release candidate 26.32.2-RC

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@
 
 import PackageDescription
 
-let version: String = "26.30.0-RC"
-let urlVersion: String = "26.30.0"
-let checksum: String = "4ab31b9397f9dcdcd17de8e27b98aeee16e78b1f723b1b311f5ecf2d19694954"
-let catnipVersion = Version(0, 0, 9)
+let version: String = "26.32.2-RC"
+let urlVersion: String = "26.32.2"
+let checksum: String = "3cbae0d85a370b2224126ac415ac99c4451b8a137a591b5c8aefc90b4d169964"
+let catnipVersion = Version(0, 0, 10)
 
 let dependencies: [Target.Dependency] = [
     .product(name: "Catnip", package: "catnip-spm"),
