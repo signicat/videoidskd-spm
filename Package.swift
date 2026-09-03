c3dd7aa2074ef74d9423ca1477889a87a8529eb0
GitLab CI <ci@electronicid.eu>
2026-09-03 14:48:27 +0200

    Release candidate 26.36.1-RC

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let version: String = "26.36.1-RC"
 let urlVersion: String = "26.36.1"
-let checksum: String = "4eb101ac13b3bdc91c862aa91879dacd07415b66979fe0486f190a7d75609aeb"
+let checksum: String = "5cb32f8db13f47817fa8ff332fe68290a457453c40a8c33b7f49783b1800580e"
 let catnipVersion = Version(0, 0, 10)
 
 let dependencies: [Target.Dependency] = [
